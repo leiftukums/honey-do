e2676b9b159dc4d385ca6f40f7aad2a5f4e894e5
Leif Tukums <leiftukums@gmail.com>
2020-10-24 10:36:00 -0400

    added uuid

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,45 +1,8 @@
 import React, { useState } from 'react';
-
-// const toDoList = [
-//     {
-//     id: 1,
-//     description: 'walk the dog'
-// },
-// {
-//     id: 2,
-//     description: 'cook dinner'
-// },
-// {
-//     id: 3,
-//     description: 'clean the floors'
-// },
-// {
-//     id: 4,
-//     description: 'cut the grass'
-// }
-// ];
-
-
-// const headerDisplay = () => {
-//     if(toDoList.length > 1){
-//         return <p>You have {toDoList.length} things to do today</p>
-//     } else if(toDoList.length = 1) {
-//         return <p>You have {toDoList.length} thing to do today</p>
-//     } else if(toDoList.length = 0) {
-//         return <p>You have nothing to do today!</p>
-//     }
-// };
-
-// const listItem = toDoList.map((item) => {
-//     const {id, description} = item;
-//     return <li key={id}><a>{item.description}</a></li>
-// });
-
-
-
+import { v4 as uuidv4 } from 'uuid';
 
 const ToDoList = () => {
-    const [id, value] = useState('')
+    const [id = uuidv4(), value] = useState('')
     const [addItem, setAddItem] = useState('');
     const [list, setList] = useState([]);
     const addListItem = (e) => {
@@ -49,28 +12,32 @@ const ToDoList = () => {
             setList((item) => {
                 return [...list, addItem];
             })
-            console.log(item);
+            console.log(addItem, ' id = ' ,uuidv4());
             setAddItem('');
         } else {
             alert('Please add a task')
         }
-    }
+    };
 
     return (
         <div>
             <h1>Honey Do</h1>
             <h4>You may think you can just relax today, but you have {list.length} things to do...</h4>
-            <h5>Better get started!</h5>
+            <h4>Better get started!</h4>
+            <ul>
             {
-                list.map((item) => {
-                    return (
-                        <div>
-                        <p>{item}</p>
-                        </div>
-                    )
-                })
-            }
+                    list.map((item) => {
+                        return (
+                            <ul key={uuidv4()}>
+                                <li>{item}</li>
+                            </ul>
+                        )
+                    })
+                }
+            </ul>
+                
             <p>Oh, don't forget...</p>
+            <p></p>
             <label>Add a task : </label>
             <input type='text' value={addItem} onChange={(e) => setAddItem(e.target.value)}></input>
             <button type='submit' onClick={addListItem}>Add</button>
